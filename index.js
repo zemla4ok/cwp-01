cb13dc7e22b4af79dd9af4b4a3ea40cf57232a0c
zemla4ok <dima-vital@live.com>
2017-09-18 15:49:37 +0300

    print all command line arguments

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const name = process.argv[2];
+const surname = process.argv[3];
+const city = process.argv[4];
 
-console.log(`Hi ${name}!`);
+console.log(`Hi, i'm ${name} ${surname}. I'm from ${city}!`);
+
