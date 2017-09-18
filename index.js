4eb98c03e966ce3022a8168984af438a9c44bd89
zemla4ok <dima-vital@live.com>
2017-09-18 20:28:56 +0300

    function for copy file(beta)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var path = require('path');
 
 const DIR_PATH = process.argv[2];
+const EXTENSION = '*.txt';
 
 let myScriptForSummary =
     'const fs = require(\'fs\');\n' +
@@ -63,12 +64,40 @@ function setCopyright() {
 
 function createDirForTXT() {
     let dir = `${DIR_PATH}\\${path.basename(DIR_PATH)}`;
-    fs.mkdir(dir,(err) => {
-        if(err){
+    fs.mkdir(dir, (err) => {
+        if (err) {
             console.log(err);
             console.log("error in creatind directory for *.txt files");
             throw  err;
         }
     });
     return dir;
+}
+
+function copyTXT(dir, dirOfTXTFiles) {
+    fs.readdir(dir, function (err, files) {
+        if (err)
+            console.log(err);
+        else {
+            for(let file in files){
+                let currentFile = `${dir}\\${files[file]}`;
+                if(fs.stat(currentFile).isDirectory()){
+                    copyTXT(currentFile, dirOfTXTFiles);
+                }
+                else{
+                    if(path.extname(currentFile) === EXTENSION){
+                        fs.readFile(currentFile, 'utf8', (err, data) => {
+                            if(err){
+                                console.log(err);
+                                console.log(`can't read file ${currentFile}`);
+                            }
+                            else{
+
+                            }
+                        })
+                    }
+                }
+            }
+        }
+    })
 }
