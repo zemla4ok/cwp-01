4773db390a0428481f9c63fb241fd52c176af949
zemla4ok <dima-vital@live.com>
2017-09-18 16:47:34 +0300

    creating file

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const name = process.argv[2];
-const surname = process.argv[3];
-const city = process.argv[4];
+var fs = require('fs');
+const DIR_PATH = process.argv[2];
 
-console.log(`Hi, i'm ${name} ${surname}. I'm from ${city}!`);
-
+fs.appendFile(`${DIR_PATH}\\summary.js`, 'my file');
